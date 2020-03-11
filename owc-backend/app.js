ed7aae6361c69248fc1f342681eb6ba50d56e72c
David Wampamba <davidwampamba@gmail.com>
2020-03-12 02:11:46 +0300

    Database configuration

--- owc-backend/app.js
+++ owc-backend/app.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require('express')
+require('./db/db')
+const userRouter = require('./routes/users')
+const expenseRouter = require('./routes/expenses')
+const PORT = process.env.PORT
+
 const app = express();
+
+app.use(express.json())
+app.use(userRouter)
+app.use(expenseRouter)
 
 app.get('/', (req, res) => {
   res.send('Oooops!!')
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('One week challenge server is running on port 3000');
 });
