4d9df6d3c2257693de36ed36e58974ac6d76b92d
David Wampamba <davidwampamba@gmail.com>
2020-03-13 00:20:41 +0300

    New: users/logout, users/logout-all, /users/delete, users/:id routes

--- owc-backend/routes/users.js
+++ owc-backend/routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const User = require('../models/user')
-
+const auth = require('../middleware/auth')
 const router = express.Router()
 
-router.post('/users', async function(req, res){
+router.post('/users', async function(req, res) {
     try {
         const user = new User(req.body)
         await user.save()
@@ -28,6 +28,41 @@ router.post('/users/login', async function(req, res){
     } catch (error) {
         res.status(400).send(error)
     }
+})
+
+router.get('/users/profile', auth, async (req, res) => {
+
+    res.send(req.user)
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter( token => token.token != req.token)
+        await req.user.save()
+        res.send()
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.post('/users/logout-all', auth, async (req, res) => {
+    try {
+        req.user.tokens.splice(0, token => req.user.tokens.length)
+        await req.user.save()
+        res.send()
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.put('/users/:id', auth, async (req, res) => {
+
+})
+
+router.delete('/users/delete/:id', auth, async (req, res) => {
+
 })
 
 module.exports = router
