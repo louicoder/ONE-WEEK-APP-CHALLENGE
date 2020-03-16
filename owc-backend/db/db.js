da0d6c1aa9034960103f7f4cac2275289088cf10
David Wampamba <davidwampamba@gmail.com>
2020-03-16 11:28:25 +0300

    Refactoring

--- owc-backend/db/db.js
+++ owc-backend/db/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 //'mongodb+srv://owc:KsSlsCy7uptG2FZT@cluster0-vlond.mongodb.net/test?retryWrites=true&w=majority'
-
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
