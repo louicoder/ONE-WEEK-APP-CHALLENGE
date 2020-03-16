a8f11b2f4a127b08d36fbfecdb4105fc789e2de4
David Wampamba <davidwampamba@gmail.com>
2020-03-16 11:19:08 +0300

    Bug fix: Replaced hardcoded Mongodb URL with one in the .env file

--- owc-backend/db/db.js
+++ owc-backend/db/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb+srv://owc:KsSlsCy7uptG2FZT@cluster0-vlond.mongodb.net/test?retryWrites=true&w=majority', {
+//'mongodb+srv://owc:KsSlsCy7uptG2FZT@cluster0-vlond.mongodb.net/test?retryWrites=true&w=majority'
+
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
